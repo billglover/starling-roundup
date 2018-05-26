286492fe3f89ebe60391c3f76098e909cb54c796
Bill Glover <bill@billglover.co.uk>
2018-05-26 23:45:26 +0100

    Request transaction details

--- main.go
+++ main.go
@@ -1,58 +1,101 @@
 package main
 
 import (
+	"context"
+	"crypto/sha512"
+	"encoding/base64"
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
+
+	"github.com/billglover/starling"
+	"golang.org/x/oauth2"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
-
-type Amount struct {
-	Currency   string `json:"currency"`
-	MinorUnits int    `json:"minorUnits"`
-}
 
 type WebHookPayload struct {
 	WebhookNotificationUID string    `json:"webhookNotificationUid"`
-	CustomerUID            string    `json:"customerUid"`
-	WebhookType            string    `json:"webhookType"`
-	EventUID               string    `json:"eventUid"`
-	TransactionAmount      Amount    `json:"transactionAmount"`
-	SourceAmount           Amount    `json:"sourceAmount"`
-	Direction              string    `json:"direction"`
-	Description            string    `json:"description"`
-	MerchantUID            string    `json:"merchantUid"`
-	MerchantLocationUID    string    `json:"merchantLocationUid"`
-	Status                 string    `json:"status"`
-	TransactionMethod      string    `json:"transactionMethod"`
-	TransactionTimestamp   time.Time `json:"transactionTimestamp"`
-	MerchantPosData        struct {
-		PosTimestamp       string `json:"posTimestamp"`
-		CardLast4          string `json:"cardLast4"`
-		AuthorisationCode  string `json:"authorisationCode"`
-		Country            string `json:"country"`
-		MerchantIdentifier string `json:"merchantIdentifier"`
-	} `json:"merchantPosData"`
+	Timestamp              time.Time `json:"timestamp"`
+	Content                struct {
+		Class          string `json:"class"`
+		TransactionUID string `json:"transactionUid"`
+		Amount         int    `json:"amount"`
+		SourceCurrency string `json:"sourceCurrency"`
+		SourceAmount   int    `json:"sourceAmount"`
+		CounterParty   string `json:"counterParty"`
+		Reference      string `json:"reference"`
+		Type           string `json:"type"`
+		ForCustomer    string `json:"forCustomer"`
+	} `json:"content"`
+	AccountHolderUID string `json:"accountHolderUid"`
+	WebhookType      string `json:"webhookType"`
+	CustomerUID      string `json:"customerUid"`
+	UID              string `json:"uid"`
 }
 
 func handle(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// Fetch the Personal Webhook Secret from the Parameter Store
+	svc := ssm.New(session.New())
+	swh := "starling-webhook-secret"
+	spt := "starling-personal-token"
+
+	dec := true
+	paramsIn := ssm.GetParametersInput{
+		Names:          []*string{&swh, &spt},
+		WithDecryption: &dec,
+	}
+
+	paramsOut, err := svc.GetParameters(&paramsIn)
+	if err != nil {
+		log.Println(err)
+		return serverError(err)
+	}
+	params := make(map[string]string, len(paramsOut.Parameters))
+	for _, p := range paramsOut.Parameters {
+		params[*p.Name] = *p.Value
+	}
+
+	secret := params[swh]
+	token := params[spt]
+
+	// Calculate the request signature
+	sha512 := sha512.New()
+	sha512.Write([]byte(secret + request.Body))
+	recSig := base64.StdEncoding.EncodeToString(sha512.Sum(nil))
+	reqSig := request.Headers["X-Hook-Signature"]
+
+	// Reject the request if it doesn't match the signature header
+	if reqSig != recSig {
+		log.Println("WARN: invalid request signature received")
+		return clientError(http.StatusBadRequest)
+	}
+
 	wh := new(WebHookPayload)
-	err := json.Unmarshal([]byte(request.Body), &wh)
+	err = json.Unmarshal([]byte(request.Body), &wh)
 	if err != nil {
 		return serverError(err)
 	}
 
-	sig := request.Headers["X-Hook-Signature"]
-	log.Println("Signature:", sig)
-	log.Println("Body:", request.Body)
-
 	log.Println("Customer UID:", wh.CustomerUID)
-	log.Println("Method", wh.TransactionMethod)
-	log.Println("Amount", wh.TransactionAmount.MinorUnits, wh.TransactionAmount.Currency)
-	log.Println("Time", wh.TransactionTimestamp.Format(time.RFC3339Nano))
+	log.Println("Type:", wh.Content.Type)
+	log.Println("Amount:", wh.Content.Amount)
+	log.Println("Time:", wh.Timestamp.Format(time.RFC3339Nano))
+
+	ctx := context.Background()
+	sb := newClient(ctx, token)
+	txn, _, err := sb.Transaction(ctx, wh.Content.TransactionUID)
+	if err != nil {
+		return serverError(err)
+	}
+
+	log.Println("Narrative:", txn.Narrative)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
@@ -76,4 +119,13 @@ func clientError(status int) (events.APIGatewayProxyResponse, error) {
 
 func main() {
 	lambda.Start(handle)
+}
+
+func newClient(ctx context.Context, token string) *starling.Client {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+
+	baseURL, _ := url.Parse(starling.ProdURL)
+	opts := starling.ClientOptions{BaseURL: baseURL}
+	return starling.NewClientWithOptions(tc, opts)
 }
